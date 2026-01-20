1bf48422e93debcd9dd8d16faf3969e78f7b9c45
NivinCNC <nivincharles@gmail.com>
2026-01-20 23:07:10 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,8 @@ subprojects {
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
+                    "-Xno-receiver-assertions",
+                    "-Xannotation-default-target=param-property"
                 )
             }
         }
