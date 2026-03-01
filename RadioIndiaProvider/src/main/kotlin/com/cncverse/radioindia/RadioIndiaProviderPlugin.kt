80067354a40606511edcb91961cf910f11b8dc82
NivinCNC <nivincharles@gmail.com>
2026-03-01 11:28:19 +0530

    Radio India Added

---
+++ RadioIndiaProvider/src/main/kotlin/com/cncverse/radioindia/RadioIndiaProviderPlugin.kt
@@ -0,0 +1,12 @@
+package com.cncverse.radioindia
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class RadioIndiaProviderPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(RadioIndiaProvider())
+    }
+}
